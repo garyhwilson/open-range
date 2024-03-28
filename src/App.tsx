b462ce9a03a486bf438630b4550756b0a8e34887
Gary Wilson <gary.wilson@transova.com>
2024-03-28 15:57:49 -0500

    Refactor App component and add fade-in animation options

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { OpenRange } from "../lib/main";
 
 function App() {
-  const [count, setCount] = useState(0)
+  enum fadeInSpeed {
+    NONE,
+    FAST,
+    MEDIUM,
+    SLOW,
+  }
+
+  const [visible, setVisible] = useState(false);
+  const [fadeIn, setFadeIn] = useState(0);
+
+  const handleOnClick = () => {
+    setVisible(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {visible && !fadeIn ? (
+        <OpenRange onClick={handleOnClick} />
+      ) : (
+        visible ? <OpenRange
+          onClick={handleOnClick}
+          className={`${
+            fadeIn ? `fadeIn ${fadeIn && fadeInSpeed[fadeIn].toLowerCase()}` : ""
+          }`}
+        /> : null
+      )}
+      <select onChange={(e) => setFadeIn(parseInt(e.target.value))}>
+        <option value={fadeInSpeed.NONE}>No Fade</option>
+        <option value={fadeInSpeed.FAST}>Fast</option>
+        <option value={fadeInSpeed.MEDIUM}>Medium</option>
+        <option value={fadeInSpeed.SLOW}>Slow</option>
+      </select>
+      <button onClick={() => setVisible(!visible)}>Display Open Range</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
