495fc8d5d1afae48e32b46cdcebe8369107b9f2f
Gary Wilson <gary.wilson@transova.com>
2024-03-28 15:54:49 -0500

    Add devDependencies and plugins for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,45 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import libAssetsPlugin from '@laynezh/vite-plugin-lib-assets'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ include: ['lib'] })
-  ],
+    libInjectCss(),
+    dts({ include: ['lib'] }),
+    libAssetsPlugin({
+      outputPath: (url) => {
+        return url.endsWith('.png') ? 'image' : 'assets'
+      },
+    })],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime']
-    }
-  }
-})
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob.sync('lib/**/*.{ts,tsx}').map((file) => [
+          // The name of the entry point
+          // lib/nested/foo.ts becomes nested/foo
+          relative('lib', file.slice(0, file.length - extname(file).length)),
+          // The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+});
